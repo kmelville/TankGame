e506b9060b8f6a90cb585379e889790dba71d40d
kmelville <kentmelville@gmail.com>
2014-04-17 20:26:41 -0400

    shanges

--- js/ShipMovingTouch.js
+++ js/ShipMovingTouch.js
@@ -23,7 +23,7 @@ ShipMoving = function(x,y) {
 
 	this.update = function() {
 		//speed limit
-		var maxSpeed = 30; 
+		var maxSpeed = 10; 
 		if(targetVel.isMagGreaterThan(maxSpeed)){
 			targetVel.normalise(); 
 			targetVel.multiplyEq(maxSpeed); 
