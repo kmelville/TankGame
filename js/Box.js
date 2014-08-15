021dd5f42b187cde153f4b5d9ea23282159cd4c7
kmelville <kentmelville@gmail.com>
2014-08-15 01:34:11 -0400

    test

--- js/Box.js
+++ js/Box.js
@@ -65,8 +65,8 @@ Box = function(x,y) {
             
             c.clearRect(0,0,32,32); 
             c.fillStyle = "rgba(255,255,255,0.5)";
-            if(this.exploded) return;
             c.fillRect(0,0,60,60); 
+            if(this.exploded) return;
             c.save();
             c.translate(16, 16); 
             c.drawImage(boxImg, -16, -16);
